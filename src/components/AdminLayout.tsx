86cce564e21f05ec81faee52ba08ba83c4b8d453
Gitwit22 <itstheplugllc@gmail.com>
2026-05-26 11:48:10 -0400

    add stage persistant5

--- src/components/AdminLayout.tsx
+++ src/components/AdminLayout.tsx
@@ -1,17 +1,29 @@
 import { Outlet } from "react-router-dom";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { AdminSidebar } from "./AdminSidebar";
+import { AppTopbar } from "@/components/layout/AppTopbar";
 import { DemoBanner } from "@/components/DemoBanner";
 import { useAppMode } from "@/context/AppModeContext";
+import { PlatformErrorBoundary } from "@/components/layout/PlatformErrorBoundary";
 
 export function AdminLayout() {
   const { isDemo } = useAppMode();
   return (
-    <div className="min-h-screen flex flex-col">
-      {isDemo && <DemoBanner />}
-      <div className="flex-1 flex flex-col">
-        <main className="flex-1 p-6 overflow-auto">
-          <Outlet />
-        </main>
+    <SidebarProvider>
+      <div className="min-h-screen flex w-full">
+        <PlatformErrorBoundary>
+          <AdminSidebar />
+        </PlatformErrorBoundary>
+        <div className="flex-1 flex flex-col">
+          {isDemo && <DemoBanner />}
+          <PlatformErrorBoundary>
+            <AppTopbar readonlyHint="Client viewers can review activity but cannot make changes." />
+          </PlatformErrorBoundary>
+          <main className="flex-1 p-6 overflow-auto">
+            <Outlet />
+          </main>
+        </div>
       </div>
-    </div>
+    </SidebarProvider>
   );
 }
